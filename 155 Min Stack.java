006a5b493d72ee34295844f43dc44306b499e81e
justinzhangcolumbia <125292032@qq.com>
2018-06-13 14:56:51 +0800

    Create 155 Min Stack.java

---
+++ 155 Min Stack.java
@@ -0,0 +1,38 @@
+class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> s;
+    Stack<Integer> minStack;
+    public MinStack() {
+        s = new Stack();
+        minStack = new Stack();
+    }
+    
+    public void push(int x) {
+        s.push(x);
+        int minvalue= minStack.isEmpty()? x : Math.min(x,minStack.peek());
+        minStack.push(minvalue);
+    }
+    
+    public void pop() {
+        s.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
